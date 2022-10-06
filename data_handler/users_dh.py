801bed134fb7f29eb5e9a1b4c464aa07988a9dd8
Aneta Wawer <anwawer1@gmail.com>
2022-10-06 13:27:02 +0200

    navbar, main_page done

--- data_handler/users_dh.py
+++ data_handler/users_dh.py
@@ -6,6 +6,7 @@ from typing import List, Dict
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 import database_common
+from flask import session
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -25,13 +26,13 @@ def get_all_users(cursor):
     return cursor.fetchall()
 
 
-
 @database_common.connection_handler
 def get_user_name(cursor, user_id):
     query="""SELECT users.login FROM users WHERE id = %(user_id)s """
     user_id={'user_id':user_id}
     cursor.execute(query,user_id)
     return cursor.fetchall()
+
 
 @database_common.connection_handler
 def get_user_details(cursor, user_id):
@@ -43,12 +44,14 @@ def get_user_details(cursor, user_id):
     cursor.execute(query,user_id)
     return cursor.fetchall()
 
+
 @database_common.connection_handler
 def get_question_by_user_id(cursor, user_id):
     query = """SELECT question.id, question.title FROM question WHERE user_id = %(user_id)s"""
     user_id={'user_id':user_id}
     cursor.execute(query,user_id)
     return cursor.fetchall()
+
 
 @database_common.connection_handler
 def get_answer_by_user_id(cursor, user_id):
@@ -57,12 +60,14 @@ def get_answer_by_user_id(cursor, user_id):
     cursor.execute(query,user_id)
     return cursor.fetchall()
 
+
 @database_common.connection_handler
 def get_comment_by_user_id(cursor, user_id):
     query = """SELECT comment.question_id, comment.answer_id, comment.message FROM comment WHERE user_id = %(user_id)s"""
     user_id={'user_id':user_id}
     cursor.execute(query,user_id)
     return cursor.fetchall()
+
 
 @database_common.connection_handler
 def update_number_of_user_questions(cursor, user_id):
@@ -78,6 +83,7 @@ def update_number_of_user_answers(cursor, user_id):
     user_id={'user_id':user_id}
     cursor.execute(query,user_id)
 
+
 @database_common.connection_handler
 def update_number_of_user_comments(cursor, user_id):
     query = """UPDATE user_details 
@@ -85,11 +91,13 @@ def update_number_of_user_comments(cursor, user_id):
     user_id = {'user_id': user_id}
     cursor.execute(query, user_id)
 
+
 @database_common.connection_handler
 def get_user_id(cursor):
     query = """SELECT user_id FROM user_details"""
     cursor.execute(query)
     return cursor.fetchall()
+
 
 @database_common.connection_handler
 def get_user_id_in_questions(cursor, id):
@@ -100,6 +108,7 @@ def get_user_id_in_questions(cursor, id):
     """
     cursor.execute(query, {'question_id':id})
     return cursor.fetchone()
+
 
 @database_common.connection_handler
 def get_user_id_by_login(cursor, login):
@@ -123,10 +132,12 @@ def reputation_for_questions_down(cursor, user_id ):
     query="""UPDATE user_details SET reputation = reputation -2 WHERE user_id= %(user_id)s"""
     cursor.execute(query, {"user_id": user_id})
 
+
 @database_common.connection_handler
 def reputation_for_answers_up(cursor, user_id):
     query = """UPDATE user_details SET reputation = reputation + 10 WHERE user_id= %(user_id)s"""
     cursor.execute(query, {"user_id": user_id})
+
 
 @database_common.connection_handler
 def reputation_for_accepted_answer_up(cursor, user_id):
@@ -139,14 +150,33 @@ def reputation_for_answers_down(cursor, user_id):
     query = """UPDATE user_details SET reputation = reputation - 2 WHERE user_id= %(user_id)s"""
     cursor.execute(query, {"user_id": user_id})
 
+
 @database_common.connection_handler
 def get_user_id_by_question_id(cursor, question_id):
     query ="""SELECT user_id FROM question WHERE id= %(question_id)s"""
     cursor.execute(query, {'question_id':question_id})
     return cursor.fetchone()
 
+
 @database_common.connection_handler
 def get_user_id_by_answer_id(cursor, answer_id):
     query ="""SELECT user_id FROM answer WHERE id= %(answer_id)s"""
     cursor.execute(query, {'answer_id':answer_id})
     return cursor.fetchone()
+
+
+def is_user_logged_in_message():
+    if session.get('is_logged_in'):
+        message = 'Logged in as: ' + session['user_login']
+    else:
+        message = 'You are not logged in.'
+    return message
+
+
+def user_id():
+    if session.get('is_logged_in'):
+        user = get_user_id_by_login(session['user_login'])
+        user_id = user['id']
+        return user_id
+    else:
+        pass
